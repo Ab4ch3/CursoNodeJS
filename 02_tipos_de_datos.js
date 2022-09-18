49007269cadf3afb51846a49688967a3c7c461ff
Miguel Abache <miiguel.abache@gmail.com>
2022-09-17 22:08:26 -0400

    subiendo archivos

---
+++ 02_tipos_de_datos.js
@@ -0,0 +1,39 @@
+// Comentario de una linea
+/* Comentario
+  de Multiples 
+  lineas
+*/
+
+//numero - number
+console.log(27);
+console.log(4.36);
+
+//cadena - string
+console.log("Hola");
+console.log("migue");
+console.log(`Miguel Abache`);
+
+//Valores logicos
+console.log(true);
+console.log(false);
+
+//undefined
+console.log(undefined);
+
+//null
+console.log(null);
+
+/*  Tipos de 
+    datos Estructurales 
+*/
+
+//Objeto
+console.log({
+  nombre: "miguel",
+  apellido: "abache",
+  edad: 27,
+  fechaNacimiento: "07-06-92",
+});
+
+//Arrays
+console.log([1, true, "hola", ["A", "B", "C"]]);
