437dc5acc924df2962e370784b4844462e6b7259
Miguel Abache <miiguel.abache@gmail.com>
2022-11-26 00:01:28 -0400

    subiendo cositas

--- servidores_web/index.js
+++ servidores_web/index.js
@@ -1,12 +1,12 @@
 // Es el archivo que tendra todo nuestro servidor
 const express = require("express");
-const SERVICE = require("./src/services.js");
+const SERVICE = require("./src/services");
 
 // Desde este paso ya la app puede recibir peticiones
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 // Nos permite revibir datos tipo json a nuestro servidor
-app.use(express.json);
+app.use(express.json());
 
 //aca la peticiones que vamos a escuchar en el servidor
 //Cada uno recibe (ruta , controlador de ruta)
@@ -17,18 +17,47 @@ app.get("/", (req, res) => {
     body: SERVICE.getUsers(),
   });
 });
+// get recibiendo parametros
+app.get("/:id", (req, res) => {
+  // Estamos sacando de request del objeto params el id que le estamos pasando a la ruta
+  let {
+    params: { id },
+  } = req;
+  let user = SERVICE.getUserByID(id);
+  res.json({
+    message: `Usuario ${id}`,
+    body: user,
+  });
+});
 
-/* app.post("/create", (req, res) => {
+app.post("/", (req, res) => {
   // metemos el cuerpo del request con los datos que recibimos en una variable
   // Usamos dessectructuracion de objeto para sacar el body del request y le asiganamos el nombre de newUser
   let { body: newUser } = req;
   let user = SERVICE.createUser(newUser);
   // Le colocamos la respuesta que va a recibir la peticion cuando este hecha
-  res.json({
+  // Podemos cambiar el tipo de codigo que va a enviar el servidor como respuesta
+  res.status(201).json({
     message: "Usuario fue creado Exitosamente",
     body: user,
   });
-}); */
+});
+
+app.put("/:id", (req, res) => {
+  //Put para modificar algun dato
+});
+
+app.delete("/:id", (req, res) => {
+  //delete para eliminar un dato
+  let {
+    params: { id },
+  } = req;
+  let userDeleted = SERVICE.delete(id);
+  res.json({
+    message: `El Usuario a sido Eliminado`,
+    body: userDeleted,
+  });
+});
 
 //Para recibir peticiones hay que levantar el servidor y escuchar las peticiones
 //Recibe un puerto , y un callback que nos indica cuando esta escuchando
--- servidores_web/src/services.js
+++ servidores_web/src/services.js
@@ -6,7 +6,13 @@ const data = require("./MOCK_DATA.json");
 // Le estamos diciendo al archivo service que va a exportar funciones que seran usadas en otros archivos
 module.exports = {
   getUsers: () => data,
-  /*  createUser: (dataUser) => {
+  getUserByID: (id) => {
+    // recordar que siempre que recibimos parametros recibimos strings , en caso tal debemos hacer la conversion
+    let idPerson = Number(id);
+    let user = data.find((person) => person.id === idPerson);
+    return user;
+  },
+  createUser: (dataUser) => {
     let newUser = {
       id: data.length + 1,
       ...dataUser,
@@ -15,5 +21,11 @@ module.exports = {
     data.push(newUser);
     // Va a retornar el ultimo usuario creado
     return newUser;
-  }, */
+  },
+  delete: (id) => {
+    let idPerson = Number(id);
+    let findIndex = data.findIndex((person) => person.id === idPerson);
+    let result = data.splice(findIndex, 1);
+    return result;
+  },
 };
